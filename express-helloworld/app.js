dd26f79d392a8d6678a06b6b9d9ab8206112f920
0008537 <ejsaldana@viewnext.com>
2020-02-01 18:15:43 +0100

    delete moon service

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -9,11 +9,7 @@ app.get('/mars', function(req, res) {
   res.send('Hello Mars!\n');
 });
 
-app.get('/moon', function(req, res) {
-  res.send('Hello Moon!\n');
-});
-
-app.listen(8081, function () {
-  console.log('Example app listening on port 8081!');
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
 });
 
